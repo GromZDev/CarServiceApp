1ee3ee5712207db2fbcb89b93d0a1d33604854e4
GromZDev <Dmitryev@mail.ru>
2021-12-27 20:31:15 +0300

    Сделал экраны для смены пароля. И логику перехода по ним. Также и в main_navigation.xml.

--- app/src/main/java/carService/app/ui/auth/ForgotPasswordStep2Fragment.kt
+++ app/src/main/java/carService/app/ui/auth/ForgotPasswordStep2Fragment.kt
@@ -5,18 +5,17 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import carService.app.R
-import carService.app.databinding.ForgotPasswordFragmentBinding
-import carService.app.databinding.RegistrationStep3FragmentBinding
+import carService.app.databinding.ForgotPasswordStep2FragmentBinding
 import carService.app.utils.hideToolbarAndBottomNav
 import carService.app.utils.navigate
 
-class ForgotPasswordFragment : Fragment(R.layout.forgot_password_fragment) {
+class ForgotPasswordStep2Fragment : Fragment(R.layout.forgot_password_step2_fragment) {
 
     companion object {
-        fun newInstance() = ForgotPasswordFragment()
+        fun newInstance() = ForgotPasswordStep2Fragment()
     }
 
-    private val binding: ForgotPasswordFragmentBinding by viewBinding()
+    private val binding: ForgotPasswordStep2FragmentBinding by viewBinding()
     private lateinit var viewModel: ForgotPasswordViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -24,12 +23,12 @@ class ForgotPasswordFragment : Fragment(R.layout.forgot_password_fragment) {
 
         hideToolbarAndBottomNav()
 
-        binding.nextCreateAccountButton.setOnClickListener {
-            navigate(R.id.forgotPasswordStep2Fragment)
+        binding.nextResetPasswordButton.setOnClickListener {
+            navigate(R.id.forgotPasswordStep3SuccessFragment)
         }
 
         binding.backButtonImage.setOnClickListener {
-            navigate(R.id.loginFragment)
+            navigate(R.id.forgotPasswordFragment)
         }
     }
 }
