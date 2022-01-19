517d82df564ca96663b1dbfd729db54ee6591b22
GromZDev <Dmitryev@mail.ru>
2022-01-19 16:23:45 +0300

    Добавил экран с вводом пароля.

--- app/src/main/java/carService/app/ui/auth/ForgotPasswordFragment.kt
+++ app/src/main/java/carService/app/ui/auth/ForgotPasswordFragment.kt
@@ -3,15 +3,9 @@ package carService.app.ui.auth
 import carService.app.R
 import carService.app.base.BaseFragment
 import carService.app.databinding.ForgotPasswordFragmentBinding
-import carService.app.utils.CommonConstants.USER
 import carService.app.utils.hideToolbarAndBottomNav
 import carService.app.utils.navigate
-import carService.app.utils.showsnackBar
-import kotlinx.coroutines.flow.collect
-import org.koin.androidx.viewmodel.ext.android.viewModel
-import org.koin.core.component.KoinApiExtension
 
-@KoinApiExtension
 class ForgotPasswordFragment(override val layoutId: Int = R.layout.forgot_password_fragment) :
     BaseFragment<ForgotPasswordFragmentBinding>() {
 
@@ -19,16 +13,15 @@ class ForgotPasswordFragment(override val layoutId: Int = R.layout.forgot_passwo
         fun newInstance() = ForgotPasswordFragment()
     }
 
-    private val vm by viewModel<ForgotPasswordViewModel>()
+    //  private val vm by viewModel<ForgotPasswordViewModel>()
 
-    @KoinApiExtension
     override fun initViews() {
         super.initViews()
         hideToolbarAndBottomNav()
 
-        binding.nextCreateAccountButton.setOnClickListener {
-            navigate(R.id.forgotPasswordStep2Fragment)
-        }
+//        binding.nextCreateAccountButton.setOnClickListener {
+//          //  navigate(R.id.forgotPasswordStep2Fragment)
+//        }
 
         binding.backButtonImage.setOnClickListener {
             navigate(R.id.loginFragment)
@@ -38,31 +31,31 @@ class ForgotPasswordFragment(override val layoutId: Int = R.layout.forgot_passwo
 
         }
 
-        binding.resetPasswordEmailHint.setOnClickListener {
-            USER?.let { it1 -> vm.sendPasswordResetEmail(it1.email) }
+        binding.resetViaEmailButton.setOnClickListener {
+            navigate(R.id.forgotPasswordStepEnterEmail)
         }
     }
 
-    override fun initViewModel() {
-        doInScope {
-            vm.isSendPasswordResetEmail.collect { isSendPasswordResetEmail ->
-                if (isSendPasswordResetEmail) {
-                   view?.showsnackBar(getString(R.string.approve_sms))
-                    navigate(R.id.forgotPasswordStep2Fragment)
-                }
-            }
-            vm.isStateException.collect { isStateException ->
-                if (isStateException !="") {
-                    view?.showsnackBar(getString(R.string.failed_send_sms))
-                }
-            }
-        }
-        doInScopeResume {
-            vm.isStateException.collect { isStateException ->
-                if (isStateException !="") {
-                    view?.showsnackBar(getString(R.string.failed_send_sms))
-                }
-            }
-        }
-    }
+//    override fun initViewModel() {
+//        doInScope {
+//            vm.isSendPasswordResetEmail.collect { isSendPasswordResetEmail ->
+//                if (isSendPasswordResetEmail) {
+//                   view?.showsnackBar(getString(R.string.approve_sms))
+//                    navigate(R.id.forgotPasswordStepEnterEmail)
+//                }
+//            }
+//            vm.isStateException.collect { isStateException ->
+//                if (isStateException !="") {
+//                    view?.showsnackBar(getString(R.string.failed_send_sms))
+//                }
+//            }
+//        }
+//        doInScopeResume {
+//            vm.isStateException.collect { isStateException ->
+//                if (isStateException !="") {
+//                    view?.showsnackBar(getString(R.string.failed_send_sms))
+//                }
+//            }
+//        }
+//    }
 }
--- app/src/main/java/carService/app/ui/auth/ForgotPasswordStep4FragmentSuccess.kt
+++ app/src/main/java/carService/app/ui/auth/ForgotPasswordStep4FragmentSuccess.kt
@@ -5,18 +5,18 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import carService.app.R
-import carService.app.databinding.ForgotPasswordStep3SuccessBinding
+import carService.app.databinding.ForgotPasswordStep4SuccessBinding
 import carService.app.utils.hideToolbarAndBottomNav
 import carService.app.utils.navigate
 
-class ForgotPasswordStep3FragmentSuccess : Fragment(R.layout.forgot_password_step3_success) {
+class ForgotPasswordStep4FragmentSuccess : Fragment(R.layout.forgot_password_step4_success) {
 
     companion object {
-        fun newInstance() = ForgotPasswordStep3FragmentSuccess()
+        fun newInstance() = ForgotPasswordStep4FragmentSuccess()
     }
 
-    private val binding: ForgotPasswordStep3SuccessBinding by viewBinding()
-    private lateinit var viewModel: ForgotPasswordViewModel
+    private val binding: ForgotPasswordStep4SuccessBinding by viewBinding()
+  //  private lateinit var viewModel: ForgotPasswordViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
