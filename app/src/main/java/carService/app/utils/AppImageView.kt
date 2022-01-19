f9e9ea9ae2bce327b21392e11564439ba91e477d
GromZDev <Dmitryev@mail.ru>
2022-01-19 14:33:39 +0300

    Сделал вывод данных из БД в профиль личного аккаунта.

--- app/src/main/java/carService/app/utils/AppImageView.kt
+++ app/src/main/java/carService/app/utils/AppImageView.kt
@@ -8,10 +8,13 @@ import carService.app.BuildConfig
 import carService.app.R
 import coil.ImageLoader
 import coil.request.LoadRequest
+import coil.transform.CircleCropTransformation
 import com.google.android.material.imageview.ShapeableImageView
+import org.koin.core.component.KoinApiExtension
 import org.koin.core.component.KoinComponent
 import java.io.File
 
+@KoinApiExtension
 class AppImageView : KoinComponent {
 
     fun getTmpFileUri(ctx: Context): Uri {
@@ -35,10 +38,31 @@ class AppImageView : KoinComponent {
                     imageView.setImageResource(R.drawable.default_photo)
                 }
             )
-//            .transformations(
-//                CircleCropTransformation()
-//            )
-            //  .size(100, 100)
+            .build()
+
+        ImageLoader(container.context).execute(request)
+    }
+
+    fun useLoadPhotoToProfile(
+        container: ShapeableImageView,
+        imageView: ImageView,
+        imageLink: String
+    ) {
+        val request = LoadRequest.Builder(container.context)
+            .data(imageLink)
+            .target(
+                onStart = {},
+                onSuccess = { result ->
+                    imageView.setImageDrawable(result)
+                },
+                onError = {
+                    imageView.setImageResource(R.drawable.default_photo)
+                }
+            )
+            .transformations(
+                CircleCropTransformation()
+            )
+            .size(140, 140)
             .build()
 
         ImageLoader(container.context).execute(request)
